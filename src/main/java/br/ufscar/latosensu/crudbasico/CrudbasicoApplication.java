b5d402bbd0d7c8cb91e45ab4a40b8ecc79bf7074
Bruno <bruno.eng.comp@gmail.com>
2018-11-28 14:58:38 -0200

    Commit inicial

---
+++ src/main/java/br/ufscar/latosensu/crudbasico/CrudbasicoApplication.java
@@ -0,0 +1,12 @@
+package br.ufscar.latosensu.crudbasico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudbasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudbasicoApplication.class, args);
+	}
+}
